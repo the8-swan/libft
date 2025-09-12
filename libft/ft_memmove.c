3ca25024bb2a87a456204b3bcf30d6b02462c435
the8-swan <hananebmd077@gmail.com>
2025-09-13 00:04:38 +0100

    memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,18 +1,25 @@
-// #include "libtf.h"
+ #include "libft.h"
 
-// void *ft_memmove(void *dst, const void *src, size_t len){
-//     size_t i;
-//     i=0;
-//     void
-//     if(src < dest){
-//         while(i<len){
-//             (i<len){
-//                 ((unsigned char*) dst)=
-//             }
-//         }
-//     }
+ void *ft_memmove(void *dst, const void *src, size_t len){
+    size_t i=0;
 
 
+    if(!dst && !src)
+        return NULL;
 
-//     return dst;
-// }
+        
+    if(((unsigned char*) src) < ((unsigned char*)dst)){
+        while(len>0){
+                len--;
+                ((unsigned char*) dst)[len]= ((unsigned char*) src)[len];
+    
+        }
+    }else{
+        while(i<len){
+            ((unsigned char*) dst)[i]= ((unsigned char*) src)[i];
+            i++;
+
+    } 
+    }
+     return dst;
+ }
